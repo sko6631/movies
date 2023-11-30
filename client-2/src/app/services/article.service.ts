d7975649abe96c0e239a94836f844a17b3c9a63b
Pierre ruibanys <pierreruibanys31@gmail.com>
2023-11-30 15:10:05 +0100

    add cors + /api

--- client-2/src/app/services/article.service.ts
+++ client-2/src/app/services/article.service.ts
@@ -3,26 +3,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Article } from '../models/article'
+import { Article } from '../models/article';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ArticlesService {
-  private apiUrl = 'https://localhost:7297';
+  private apiUrl = 'http://localhost:7297/api';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getArticles(offset: number , limit: number): Observable<Article[]> {
+  getArticles(offset: number, limit: number): Observable<Article[]> {
     const params = new HttpParams()
       .set('offset', offset.toString())
       .set('limit', limit.toString());
 
-    return this.http.get<Article[]>(`${this.apiUrl}/api/articles`, { params });
+    return this.http.get<Article[]>(`${this.apiUrl}/articles`, { params });
   }
 
   getArticle(id: number): Observable<Article> {
-    return this.http.get<Article>(`${this.apiUrl}/api/articles/${id}`);
+    return this.http.get<Article>(`${this.apiUrl}/articles/${id}`);
   }
 
   // Ajoutez des méthodes pour la création, la mise à jour et la suppression d'articles
